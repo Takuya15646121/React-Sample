8bcede9b87784ada7172e4fc070bbca3d89b8d78
神保　菜月 <jinbonatsuki@jinbonatsuki-no-MacBook.local>
2019-06-09 00:09:23 +0900

    fix text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ReactおじさんがCircleCIとコラボしました。
+          ReactApp。
         </a>
       </header>
     </div>
