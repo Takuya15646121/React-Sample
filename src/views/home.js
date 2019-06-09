511197be35fe4d319e3273d2367225720ed166ac
tarohatu <tarohatu@gmail.com>
2019-06-09 21:26:00 +0900

    add home.js

---
+++ src/views/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class HomeScreen extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>ログインページ</h1>
+          <p>ログインしてください</p>
+        </div>
+      );
+    }
+  }
--- src/views/signin.js
+++ src/views/signin.js
@@ -20,7 +20,7 @@ const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
+  signInSuccessUrl: '/home',
   // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID
